6442ba8a32f92c56f0efc13738f30761585b9f5c
FinanceWereTooBoring <bohdan.markiv18@gmail.com>
2023-12-07 11:47:49 +0100

    - finished strategy 4 +refactored

--- GroupAssignment2.py
+++ GroupAssignment2.py
@@ -6,113 +6,185 @@ import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 
 
-# Set up dictionaries
+# Costs per worker level
 costs = {5: 16000, 4: 12000, 3: 10000, 2: 7000}
 
 
-# idk if we need it, but here it is
-
-
 def likelihood_of_accepting(q, b):
+    """This function firstly calculates the likelihood of project acceptance based on 
+    the average quality and bid. And after that it uses np to get if the project
+    was actually accepted. 
+
+    Args:
+        q (float): average quality of the team assigned to the project.
+        b (float): bid per worker
+
+    Returns:
+        binary: 0 if it was simulated that the project was accepted, and 0 otherwise.
+    """
     result = 1/(1 + np.exp(-6.5 - 2*q + 0.0005*b))
     outcome = np.random.choice([0, 1], p=[1-result, result])
     return outcome
 
 
-def likelihood_of_accepting_greedy(q, b, n):
-    result = q * b * n*(1/(1 + np.exp(-6.5 - 2*q + 0.0005*b)))
-    return result
+def max_greedy_bid(param, people_requirement, q, d):
+    """This function provides the "objection function" for greedy strategy.
+    Generally speaking, this is what we are maximizing. Maybe it's possible to make it more efficient.
 
+    Args:
+        param (float): something we want to maximize.
+        pple_req (integer): amount of people needed for the project.
+        q (float): average quality of the team.
+        d (integer): length of the project.
 
-def max_greedy(param, n, q, d):
+    Returns:
+        float: the maximized bid.
+    """
     b = param
-    result = -b*n*d*(1 / (1 + np.exp(-6.5 - 2 * q + 0.0005 * b)))
+
+    # Here we start with minus, it's not a mistake. Thew thing is that scipy has minimization function(Check later code).
+    # So we basically minimizing negative outcome, aka maximizing positive one. I know it's weird, but that's how it works
+    # how this library works.
+    result = -b*people_requirement*d * \
+        (1 / (1 + np.exp(-6.5 - 2 * q + 0.0005 * b)))
     return result
 
 
 # Maybe there is a better place to put those, but idk yet.
+
+# Initial guess for the bid. necessary for scipy thing.
 initial_guess = [0]
 
-# Set bounds for optimization
+# Set bounds for optimization.
 bounds = [(-np.inf, np.inf)]
 
 
 class Simulation:
-    def __init__(self, bid=None, n=50, flow_bid=False, greedy=False):
+    """Instead of creating like shit tone of similar functions, i decided to combine the whole simulation process in one 
+    class. Maybe we will need to break it for later strategies, but it worked fine for strategies 1 - 4. 
+    """
+
+    def __init__(self, bid=None, n=50, fixed_probability=False, greedy=False):
+        """Initiate the class.
+
+        Args:
+            bid (float, optional): If bid is fixed, than we just specify it here, or None otherwise. Defaults to None.
+            n (integer, optional): Number of weeks for simulation. Defaults to 50.
+            fixed_probability (bool, optional): Indicator, if it's a fixed probability approach. Defaults to False.
+            greedy (bool, optional): Indicator, if it's a greedy pricing. Defaults to False.
+        """
         self.n = n
-        self.num_of_projects_per_month = np.random.poisson(4, size=n)
+        self.num_of_projects_per_month = np.random.poisson(4, size=self.n)
         self.employees_scenario_1 = create_workers()
         self.bid = bid
-        self.flow_bid = flow_bid
+        self.fixed_probability = fixed_probability
         self.greedy = greedy
+
+        # This one means that whenever you create Simulation object it already runs the simulation.
+        # And you directly can get all the values and plots.
         self.intitalisation()
 
-    def monthly_behaviour(self, num_of_proj):
-        # We find the sum of costs of all workers who are currently engaged in project
+    def monthly_behaviour(self, number_of_projects):
+        """This is a main function. It simulates the monthly decisions - costs, revenue calculations, 
+        workers allocations and others. Maybe it's smart to break it into smaller functions.  
+
+        Args:
+            num_of_proj (integer): amount of projects per month. It's the only simulation which is done on the higher level than months.
+
+        Returns:
+            tuple: revenues, costs and other proportions.
+        """
+        # We find the sum of costs of all workers who are currently engaged in project.
         monthly_costs = 0
-        # [costs(values["level"]) for values in employees_scenario_1.values() if values["when_will_be_available"] != 0].sum()
         for key in self.employees_scenario_1.keys():
+            # We are paying only to the working people, not all of them.
             if self.employees_scenario_1[key]["when_will_be_available"] != 0:
                 monthly_costs += costs[self.employees_scenario_1[key]["level"]]
-                # we reduce amount of "busy" periods
+                # We reduce amount of "busy" periods.
                 self.employees_scenario_1[key]["when_will_be_available"] -= 1
         monthly_revenue = 0
         missed_projects = 0
         taken_projects = 0
+        # Sorry for not doing man/woman_months, hahha. This variable will be used in utilization calculations.
         man_months = 0
-        for project in range(num_of_proj):
+
+        for project in range(number_of_projects):
+
+            # Basically, we simulate for each project the people and time requirements.
             scope_of_project = np.random.choice([3, 6], p=[0.5, 0.5])
             workers_requirement = np.random.choice(
                 [3, 4, 5, 6], p=[0.2, 0.3, 0.3, 0.2])
-            people = []
+            potential_team = []
+
             for worker, values in self.employees_scenario_1.items():
+                # We checked for each worker their availability.
                 # I just assume for now that better workers are checked first
-                if len(people) < workers_requirement:
+                if len(potential_team) < workers_requirement:
                     if values["when_will_be_available"] == 0:
-                        people.append(worker)
+                        potential_team.append(worker)
             # Check if we managed to form a team
-            if len(people) == workers_requirement:
-                # Please optimize me
+            if len(potential_team) == workers_requirement:
+
+                # Find the average quality of the team. Works surprisingly fast.
                 potential_quality = np.mean(
-                    [self.employees_scenario_1[worker]["level"] for worker in people])
+                    [self.employees_scenario_1[worker]["level"] for worker in potential_team])
+
+                # It allows to hold situations, when the bid needs to be optimized.
                 if not self.bid:
                     if not self.greedy:
+                        # Strategy 3
                         self.bid = 2000 * (2 * potential_quality + 6.5)
                     else:
-                        # It's maximizing thing for Strategy 4
-                        max_bid = minimize(max_greedy, initial_guess, bounds=bounds, args=(
+                        # It's optimization for Strategy 4
+                        max_bid = minimize(max_greedy_bid, initial_guess, bounds=bounds, args=(
                             workers_requirement, potential_quality, scope_of_project,), method='L-BFGS-B')
                         self.bid = max_bid.x[0]
+
+                # After we got to know the bid, we find out the likelihood of acceptance and if it was actually accepted.
                 is_accepted = likelihood_of_accepting(
                     q=potential_quality, b=self.bid)
-                # I am not sure if we have to calculate it like this, or just take expected values.
                 if is_accepted:
                     # If our project is actually accepted
-                    # I also assume that they start working on project from the next month. That's why the calc of costs is done on the beginning of the each month.
-                    for person in people:
+                    # I also assume that they start working on project from the next month.
+                    # That's why the calc of costs is done on the beginning of the each month.
+                    for person in potential_team:
+                        # Add their working months.
                         self.employees_scenario_1[person]["when_will_be_available"] = scope_of_project
-                        # Add only the ones who are actually in the project
                         man_months += scope_of_project
+                    # Yeah, this one is another assumption I make, which can be changed in the future, So right now
+                    # it is assumed that in month 0 we get assigned to the project and get the whole revenue for it,
+                    # while starting to paying to our workers monthly from month 1 until the end.
                     monthly_revenue += self.bid * workers_requirement * scope_of_project
                     taken_projects += 1
                 else:
-                    people = []
-            elif len(people) < workers_requirement:
-                # if we didn't manage to form a team
-                people = []
+                    # We formed the team, but project wasn't accepted.
+                    potential_team = []
+
+            elif len(potential_team) < workers_requirement:
+                # if we didn't manage to form a team.
+                potential_team = []
+                # I add missed projects only in this case, because in the assignment he says - "he proportion of unaddressed
+                # projects due to resource limitations." So we basically find, how many projects we lost only because we don't have enough people.
                 missed_projects += 1
             else:
-                # if we formed a team with more people in a team than needed - error
+                # If we formed a team with more people in a team than needed - error.
                 print("More people in a team than needed")
                 break
 
-            if self.flow_bid or self.greedy:
+            # If the bid is not fixed, we need to renew it for every project.
+            if self.fixed_probability or self.greedy:
                 self.bid = None
 
         return monthly_costs, monthly_revenue, missed_projects, taken_projects, man_months
 
     def intitalisation(self):
+        """This is orchestration function. It runs the for loop with monthly simulations and creates all the 
+        necessary metrics and values for the post simulation analysis.
+        """
+        # Costs and revenue are saved in lists, and not as an integer, because we will need them further for plots.
         self.total_costs, self.total_revenue, self.total_missed_projects, self.total_taken_projects, self.total_man_months = [], [], 0, 0, 0
+
+        # This is monthly simulation creation.
         for monthly_proj in self.num_of_projects_per_month:
             monthly_costs, monthly_revenue, missed_projects, taken_projects, man_months = self.monthly_behaviour(
                 monthly_proj)
@@ -121,24 +193,32 @@ class Simulation:
             self.total_missed_projects += missed_projects
             self.total_taken_projects += taken_projects
             self.total_man_months += man_months
+
+        # Create the total profit list.
         self.total_profit = [self.total_revenue[i] - self.total_costs[i]
                              for i in range(len(self.total_revenue))]
-        # Missed proj/all the projs
+
+        # Missed proj/all the projects. I calulate the total like this,
+        # because the proportion missed_proj/taken_proj is more than 1.
         self.proportion_of_missed_proj = self.total_missed_projects / \
             (self.num_of_projects_per_month.sum())
 
-        # Utilization of man-months
+        # Utilization of man-months out of available 40 people * 50 months.
         self.utilization_man_months = self.total_man_months/(40 * 50)
 
     def revenue_plot(self):
-        # Revenue plot
+        """Creates revenue histogram.
+        """
         ax = sn.histplot(self.total_revenue, kde=True, bins=50)
         ax.set(xlabel="Revenue", ylabel="Probability")
         plt.show()
+        # Never forget to close the plot.
+        # Otherwise each plt.show() will produce the same plot.
         plt.close()
 
     def profit_plot(self):
-        # Profit plot
+        """Creates profit histogram.
+        """
         a2 = sn.histplot(self.total_profit, kde=True, bins=50)
         a2.set(xlabel="Profit", ylabel="Probability")
         plt.show()
@@ -154,19 +234,19 @@ scenario_1.utilization_man_months
 
 # Strategy 2:
 proposed_bids = np.random.randint(100, 50000, 100)
-highest_exp_rev = 0
+highest_expected_revenue = 0
 best_bid = 0
 outcome = {}
 for bid in proposed_bids:
     scenario = Simulation(bid)
-    if np.mean(scenario.total_revenue) > highest_exp_rev:
+    if np.mean(scenario.total_revenue) > highest_expected_revenue:
         best_bid = bid
-        highest_exp_rev = np.mean(scenario.total_revenue)
+        highest_expected_revenue = np.mean(scenario.total_revenue)
     outcome[bid] = np.mean(scenario.total_revenue)
 
 # Strategy 3:
 
-strategy_3 = Simulation(n=50, flow_bid=True)
+strategy_3 = Simulation(n=50, fixed_probability=True)
 strategy_3.revenue_plot()
 strategy_3.profit_plot()
 strategy_3.proportion_of_missed_proj
